1f1553b0d5ce2ada3d95b7683cb51e2aaf57f149
MarcMG00 <marc.moli20@gmail.com>
2024-05-15 14:18:27 +0200

    Started session to add articles on session

--- src/main/java/controller/CtrlPanierServlet.java
+++ src/main/java/controller/CtrlPanierServlet.java
@@ -1,6 +1,6 @@
 package controller;
 
-
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -10,7 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class ServletAjout
@@ -20,20 +20,43 @@ public class CtrlPanierServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-	    
+	
+		// Creates new session
+		HttpSession session = request.getSession();
+				
+		// Gets the article selected
+		String idArticle = request.getParameter("checkedBoxPanier");
+		
+		// Check if article is not null
+		if(idArticle != null) {
+//			session.getAttribute(idArticleSession);
+			session.getAttribute("idArticleSession");
+			// Adds the article to the current session
+			session.setAttribute("idArticlesSession", idArticle);
+			request.setAttribute("idArticlesSession", idArticle);
+			try {
+				request.getRequestDispatcher("Panier.jsp").forward(request, response);
+			} catch (ServletException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
-	
-
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
 		// TODO Auto-generated method stub
-	
+
 	}
 
 }
