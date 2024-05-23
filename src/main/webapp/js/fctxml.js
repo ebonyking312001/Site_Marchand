4485a6f7ba5cf8da75e51cb50020e582051a28a3
Shan-vv <119540703+Shan-vv@users.noreply.github.com>
2024-05-23 18:44:35 +0200

    controle nb creneau dispo

--- src/main/webapp/js/fctxml.js
+++ src/main/webapp/js/fctxml.js
@@ -148,7 +148,7 @@ function getOpeningMagasin() {
 			var texteOpt = "";
 
 			for (i = 0; i < doc.length; i++) {
-				texteOpt += '<option value="' + doc[i].lastChild.firstChild.nodeValue + '">' + doc[i].firstChild.nodeValue + "</option>";
+				texteOpt += '<option value="' + doc[i].firstChild.nodeValue + '">' + doc[i].firstChild.nodeValue + "</option>";
 			}
 
 			var elt = document.getElementById("heureRetMag");
@@ -166,7 +166,7 @@ function getOpeningMagasin() {
 function confirmCard() {
 	// Objet XMLHttpRequest.
 	var xhr = new XMLHttpRequest();
-	const mot=document.getElementById("heureRetMag").innerText;
+	const mot=document.getElementById("heureRetMag").value;
 	console.log(mot);
 	nbC=mot.substring(mot.length-2,mot.length);
 	console.log(nbC);
