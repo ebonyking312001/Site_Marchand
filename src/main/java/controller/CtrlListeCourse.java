79b203758acb8486c3ce833d03f475e553f2324a
ebonyking <noe.maurel31@gmail.com>
2024-05-23 20:11:12 +0200

    add ce qu'il manquait

--- src/main/java/controller/CtrlListeCourse.java
+++ src/main/java/controller/CtrlListeCourse.java
@@ -56,6 +56,7 @@ public class CtrlListeCourse extends HttpServlet {
 					e.printStackTrace();
 				}
 			}
+			
 				
 		} else {
 			try {
@@ -64,7 +65,13 @@ public class CtrlListeCourse extends HttpServlet {
 				
 				ArrayList<ListeCourse> courses = ConnectionMySql.getListesCourses();
 				request.setAttribute("listeCourses", courses);
-
+				for(ListeCourse course : courses) {
+					int idListe = course.getIdListe();
+                    ArrayList<ContenuListe> contenuTypeProduit = ConnectionMySql.getContenuListeByIdForTypeProduit(idListe);
+                    ArrayList<ContenuListe> contenuArticle = ConnectionMySql.getContenuListeByIdForArticle(idListe);
+                    course.setContenuTypeProduit(contenuTypeProduit);
+                    course.setContenuArticle(contenuArticle);
+				}
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			} catch (SQLException e) {
